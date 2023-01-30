3edbdf0db1238db02a8f2ab02587e9f2dc220fff
“Programmer-Mehadi” <mehadi.developer@gmail.com>
2023-01-30 22:14:24 +0600

    Project Setup.

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { Provider } from 'react-redux';
+import { RouterProvider } from 'react-router-dom';
+import './App.css';
+import store from './redux/store';
+import router from './routes/router';
+
+function App() {
+  return (
+    <div className="App">
+      <Provider store={store}>
+        <RouterProvider router={router}></RouterProvider>
+      </Provider>
+    </div>
+  );
+}
+
+export default App;
